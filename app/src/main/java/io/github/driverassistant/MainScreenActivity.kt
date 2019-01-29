bf3e0edc3d73b41aa55e2504d1aa07fb149fa0a8
SerVB <servbul@yandex.ru>
2019-01-30 00:28:08 +0300

    Clean up code

--- app/src/main/java/io/github/driverassistant/MainScreenActivity.kt
+++ app/src/main/java/io/github/driverassistant/MainScreenActivity.kt
@@ -206,18 +206,11 @@ class MainScreenActivity : AppCompatActivity() {
         super.onWindowFocusChanged(hasFocus)
 
         if (hasFocus) {
-            /* Auto hide status and navigation bars: */
-            window.decorView.systemUiVisibility =
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                    View.SYSTEM_UI_FLAG_FULLSCREEN or
-                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            window.decorView.hideStatusAndNavigationBars(hasFocus)
         }
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
         when (RequestCode.values()[requestCode]) {
--- app/src/main/java/io/github/driverassistant/util/View.kt
+++ app/src/main/java/io/github/driverassistant/util/View.kt
@@ -1,7 +1,24 @@
 package io.github.driverassistant.util
 
 import android.view.View
+import android.view.View.*
 
 fun <ViewType : View> ViewType.postApply(block: ViewType.() -> Unit) {
     this.post { this.block() }
+}
+
+private const val VISIBILITY_WITHOUT_BARS =
+        SYSTEM_UI_FLAG_LAYOUT_STABLE or
+        SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+        SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+        SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+        SYSTEM_UI_FLAG_FULLSCREEN or
+        SYSTEM_UI_FLAG_HIDE_NAVIGATION
+
+fun View.hideStatusAndNavigationBars(hasFocus: Boolean) {
+    if (hasFocus) {
+        this.postApply {
+            systemUiVisibility = VISIBILITY_WITHOUT_BARS
+        }
+    }
 }
